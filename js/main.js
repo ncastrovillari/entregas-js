a245dcf508c6905eb7cbd87318f8685b4f959617
nicolas castrovillari <ncastrovillari@gmail.com>
2022-07-18 11:39:36 -0300

    Simulador

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+function precioCuotas(precio, cuotas) {
+    return precio = ((precio / cuotas))
+}
+let cuota = precioCuotas(parseFloat(prompt("Ingrese precio a pagar"))
+    , parseFloat(prompt("Ingrese cantidad de cuotas")));
+alert("Valor de la cuota es: " + cuota);
+
